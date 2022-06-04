35548e8167397366ea059343562ce0405fd66cf4
DanielFit <104741488+DanielFit@users.noreply.github.com>
2022-06-04 15:33:37 +1000

    test

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import competitors
 import current_stock
 import  analysis
 
-""""ou can use the functions to check prices of current stocked item.
+""""You can use the functions to check prices of current stocked item.
 try by_competition or by_product"""
 
 
-print(beat_it_by_10.by_competition(current_stock.outdoor_furniture,competitors.comp_outdoor_furniture))
-#print(beat_it_by_10.by_product("oak",70,current_stock.timber))
+
 print(analysis.loss_on_discount(current_stock.outdoor_furniture,competitors.comp_outdoor_furniture))
 
